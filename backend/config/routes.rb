eba110bcf1e49e4b563758c1fc41cb0554c4df59
koarakko5555 <hosayaan.1016@gmail.com>
2025-04-21 07:22:21 +0900

    devise追加

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,6 @@
 # backend/config/routes.rb
 Rails.application.routes.draw do
+  devise_for :users
   namespace :api do
     namespace :v1 do
       resources :diaries, only: [:index, :show, :create, :update, :destroy]
